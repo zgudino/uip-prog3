7c5610ca7098c2c5874c4362a150188c1015ce0f
Zahir Gudiño <zahir.gudino@gmail.com>
2015-09-22 00:17:53 -0500

    Commit de Tarea #1

---
+++ tareas/hw.1.py
@@ -0,0 +1,8 @@
+# Tarea #1
+
+speed = 120
+distance = 60
+time = distance / speed * 3600
+
+# @note Aplico tuple + interpolacion por pereza xD
+print("Necesita %dseg para recorrer %skm a %skm/h" % (time, distance, speed))
